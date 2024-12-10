6ddd5277b844e5da47de869329ffa4884ecfbad9
Rebeca Moreno Rodriguez <morenreb@oregonstate.edu>
2024-12-09 17:55:05 -0800

    Updated

--- server.js
+++ server.js
@@ -2,12 +2,30 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const outfitData = require("./public/outfits.json")
+const closetData = require("./public/closet.json")
+
 // Serve static files (index.html, style.css, script.js)
 app.use(express.static('public'));
 
-app.get('/data', (req, res) => {
-  const data = [{ id: 1, title: 'Post 1', content: 'Content of post 1' }];
-  res.json(data);  // Sends JSON data to the client
+// Endpoint to send outfit data
+app.get('/outfits', (req, res) => {
+  res.json(outfitData);
+});
+
+// Endpoint to send closet data
+app.get('/closet', (req, res) => {
+  res.json(closetData);
+});
+
+app.get('/outfits/:id', (req, res) => {
+  const outfitId = req.params.id;
+  const outfit = outfitData.find(o => o.id == outfitId);
+  
+  if (!outfit) {
+    return res.status(404).send('Outfit not found');
+  }
+  res.json(outfit);
 });
 
 // Start the server
